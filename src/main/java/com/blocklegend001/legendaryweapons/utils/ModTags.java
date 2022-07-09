d3cfff163834f3f5372526cb0c8df52263e20404
BlockCreator001 <62543902+BlockCreator001@users.noreply.github.com>
2022-07-09 17:55:27 +0200

    Add files via upload

--- src/main/java/com/blocklegend001/legendaryweapons/utils/ModTags.java
+++ src/main/java/com/blocklegend001/legendaryweapons/utils/ModTags.java
@@ -4,6 +4,7 @@ import com.blocklegend001.legendaryweapons.LegendaryWeapons;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.tags.ItemTags;
+import net.minecraft.tags.TagKey;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.common.Tags;
@@ -11,24 +12,24 @@ import net.minecraftforge.common.Tags;
 public class ModTags {
     public static class Blocks {
 
-        private static Tags.IOptionalNamedTag<Block> tag(String name) {
-            return BlockTags.createOptional(new ResourceLocation(LegendaryWeapons.MOD_ID, name));
+        private static TagKey<Block> tag(String name) {
+            return BlockTags.create(new ResourceLocation(LegendaryWeapons.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Block> forgeTag(String name) {
-            return BlockTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Block> forgeTag(String name) {
+            return BlockTags.create(new ResourceLocation("forge", name));
         }
     }
 
     public static class Items {
 
 
-        private static Tags.IOptionalNamedTag<Item> tag(String name) {
-            return ItemTags.createOptional(new ResourceLocation(LegendaryWeapons.MOD_ID, name));
+        private static TagKey<Item> tag(String name) {
+            return ItemTags.create(new ResourceLocation(LegendaryWeapons.MOD_ID, name));
         }
 
-        private static Tags.IOptionalNamedTag<Item> forgeTag(String name) {
-            return ItemTags.createOptional(new ResourceLocation("forge", name));
+        private static TagKey<Item> forgeTag(String name) {
+            return ItemTags.create(new ResourceLocation("forge", name));
         }
     }
 }
